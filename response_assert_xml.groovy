c0e17f2292a4d4e9ec3f4e2bd7579bd0d3548833
Alan <missjily@qq.com>
2015-10-09 16:42:09 +0800

    Update response_assert_xml.groovy

--- response_assert_xml.groovy
+++ response_assert_xml.groovy
@@ -4,10 +4,10 @@ def groovyUtils = new com.eviware.soapui.support.GroovyUtils( context )
 def holder = groovyUtils.getXmlHolder( "Request-xml#ResponseAsXml" )
 
 def simpleTypeClass3 = holder.getNodeValue("//urr:simpleTypeClass3")
-assert simpleTypeClass3.equals("Business_Rule_Error")
+assert simpleTypeClass3.equals("xxxxxxxxxxxx")
 
 def resultCode = holder.getNodeValue("//urr:resultCode")
-assert resultCode.equals("BRE-EIS-0009")
+assert resultCode.equals("xxxxxxxxxxxxxx")
 
 def resultDescription1=holder.getNodeValue("//urr:resultListi18nDescription//urr:descriptioni18nResult//urr:resultDescription")
-assert resultDescription1.equals("L'attribut LastName doit être présent.")
+assert resultDescription1.equals("xxxxxxxxxxxxxxxxxxxxxxxx")
